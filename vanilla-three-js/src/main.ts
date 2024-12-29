ea70e16f9818e6555b7e41d4f98be1e10920e6c9
Yaswanth Vemu <92705834+yTi7@users.noreply.github.com>
2024-12-29 11:15:00 +0000

    1.0.2 + Added a mesh to another mesh + Played with hemisphere light

--- vanilla-three-js/src/main.ts
+++ vanilla-three-js/src/main.ts
@@ -1,49 +1,57 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 
+// Get the window dimensions
+const width = window.innerWidth;
+const height = window.innerHeight;
+
 // Get the canvas
 const canvas = document.getElementById("canvas");
 
 // Create a new scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xf0f0f0);
-
-// Add a point light to the scene
-const light = new THREE.PointLight(0xffffff, 100);
-light.position.set(2, 2, 5);
-scene.add(light);
+scene.background = new THREE.Color(0x0f0f0f);
 
 // Camera settings
-const fov = 75;
-const width = window.innerWidth;
-const height = window.innerHeight;
-const near = 0.1;
-const far = 1000;
+const cameraSettings = {
+  fov: 75,
+  aspect: width / height,
+  near: 0.1,
+  far: 1000,
+};
 
-// Create a perspective camera
+// Create a perspective camera using the cameraSettings object
 const camera = new THREE.PerspectiveCamera(
-  fov,
-  width / height,
-  near,
-  far
+  cameraSettings.fov,
+  cameraSettings.aspect,
+  cameraSettings.near,
+  cameraSettings.far
 );
 camera.position.z = 5;
 scene.add(camera);
 
-// Create a cube and add it to the scene
-const cubeGeometry = new THREE.BoxGeometry();
-const cubeMaterial = new THREE.MeshStandardMaterial({
-  color: 0x00ff00,
+// Create a geometry and a material for the geometry and add it to the scene
+const icosahedronGeometry = new THREE.IcosahedronGeometry(1.0, 2); // Basically a sphere
+const icosahedronMaterial = new THREE.MeshStandardMaterial({
+  color: 0xffffff,
+  transparent: true,
+  flatShading: true,
 });
-const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-scene.add(cube);
+const icosahedron = new THREE.Mesh(icosahedronGeometry, icosahedronMaterial); // This is the final mesh
+scene.add(icosahedron);
 
-// Create a base and add it to the scene
-const baseGeometry = new THREE.BoxGeometry(2, 0.04, 1); // (width: x-axis, height: y-axis, depth: z-axis)
-const baseMaterial = new THREE.MeshStandardMaterial({ color: 0xf0f0f0 });
-const base = new THREE.Mesh(baseGeometry, baseMaterial);
-base.position.y = -0.9;
-scene.add(base);
+// Create a wireframe material
+const wireMat = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
+  wireframe: true,
+});
+const wireMesh = new THREE.Mesh(icosahedronGeometry, wireMat); // Create a wireframe mesh
+wireMesh.scale.setScalar(1.07); // Scale the wireframe mesh slightly larger than the icosahedron (sphere)
+icosahedron.add(wireMesh); // Add the wireframe to the mesh
+
+// Create a Hemisphere light that acts as a global light source (sky and ground)
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0xff0000, 1);
+scene.add(hemiLight);
 
 // Create the WebGL renderer and attach it to the canvas
 // @ts-ignore
@@ -61,9 +69,10 @@ controls.enablePan = true;
 // Animation loop
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01; // Rotate the cube on the x-axis
-  cube.rotation.y += 0.01; // Rotate the cube on the y-axis
+  // icosahedron.rotation.x += 0.01;
+  icosahedron.rotation.y += 0.01;
   renderer.render(scene, camera); // Render the scene from the perspective of the camera
+  controls.update(); // Update the controls
 }
 
 animate(); // Start the animation loop
